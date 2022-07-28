829e52ea59dc99c940904b58e13b32cb0b0bc052
Alexey Grabovenko <alexeygrabovenko@gmail.com>
2022-07-29 00:41:40 +0300

    install, connect mongoose

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/bitfilmsdb", {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App listen on port ${PORT}`);
