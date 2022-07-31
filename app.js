ec61d2ff46bcc12c25ba4d20105bd6c42dcf06f8
Alexey Grabovenko <alexeygrabovenko@gmail.com>
2022-07-31 23:17:57 +0300

    add logger and cors

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 const { errors } = require('celebrate');
+const cors = require('cors');
 const { auth } = require('./middlewares/auth');
 const userRouter = require('./routes/userRouter');
 const movieRouter = require('./routes/movieRouter');
 const mainRouter = require('./routes/index');
 const { errorHandler } = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
@@ -19,9 +21,22 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
+const corsConfig = {
+  origin: ['http://localhost:3000',
+    'http://meyer985diplom.nomoredomains.xyz',
+    'https://meyer985diplom.nomoredomains.xyz'],
+  credentials: true,
+};
+
+app.use(cors(corsConfig));
+
 app.use(mainRouter);
 app.use('/users', auth, userRouter);
 app.use('/movies', auth, movieRouter);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
