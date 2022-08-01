3af67d8bc5404ed94e5809459e6b9be48ded0bfb
Alexey Grabovenko <alexeygrabovenko@gmail.com>
2022-08-01 11:30:15 +0300

    ready for pull-request

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -1,7 +1,7 @@
 const MovieModel = require('../models/movies');
 const NotFoundError = require('../errors/NotFoundError');
 const InvalidRequest = require('../errors/InvalidRequest');
-const ForbiddenError = require('../errors/InvalidRequest');
+const ForbiddenError = require('../errors/ForbiddenError');
 
 module.exports.getMovies = (req, res, next) => {
   const { id } = req.headers;
@@ -19,13 +19,32 @@ module.exports.getMovies = (req, res, next) => {
 module.exports.postMovie = (req, res, next) => {
   const { id } = req.headers;
   const {
-    // eslint-disable-next-line max-len
-    country, director, duration, year, description, image, trailerLink, thumbnail, movieId, nameRU, nameEN,
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
   } = req.body;
 
   MovieModel.create({
-    // eslint-disable-next-line max-len
-    country, director, duration, year, description, image, trailerLink, thumbnail, movieId, nameRU, nameEN, owner: id,
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+    owner: id,
   })
     .then((result) => {
       res.status(201).send({ data: result });
@@ -39,27 +58,23 @@ module.exports.postMovie = (req, res, next) => {
     });
 };
 
-/* ПРОВЕРКА ПОЛЬЗОВАТЛЯ ПЕРЕД УДАЛЕНИЕМ */
 module.exports.deleteMovie = (req, res, next) => {
   const id = req.params._id;
   MovieModel.findById(id)
     .then((result) => {
-
-    });
-
-  MovieModel.findByIdAndDelete(id)
-    .then((result) => {
-      if (!result) {
-        throw new NotFoundError('Фильм не найден');
+      if (result.owner.toString() === req.headers.id) {
+        MovieModel.findByIdAndDelete(id)
+          .then((film) => res.status(200).send({ data: film }))
+          .catch((err) => {
+            if (err.name === 'CastError') {
+              next(new NotFoundError('Передан некорректный Id'));
+            } else {
+              next(err);
+            }
+          });
       } else {
-        res.status(200).send({ data: result });
+        throw new ForbiddenError('Ошибка доступа');
       }
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new ForbiddenError('Передан некорректный Id'));
-      } else {
-        next(err);
-      }
-    });
+    .catch(next);
 };
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,6 @@
 const { SERVER_ERROR_CODE } = require('../utils/constants');
 
 module.exports.errorHandler = (err, req, res, next) => {
-  console.log(err);
   if (!err.statusCode === '500' || !err.statusCode) {
     res.status(SERVER_ERROR_CODE).send({ message: 'Ошибка сервера' });
   } else {
